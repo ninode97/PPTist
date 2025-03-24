3debe4def48b0941c6b487911d49f403d1a40e57
ninode97 <lukas.pabijonavicius@gmail.com>
2025-03-24 15:27:22 +0200

    fix: Added automation that detects what is the canvas size based on the preloaded slide data defined in the first slide.

--- src/App.vue
+++ src/App.vue
@@ -50,6 +50,9 @@ onMounted(async () => {
     const match = window.location.pathname.match(pathPattern);
     const id = match && match[1] || ""
     api.getFileData(id).then((slides: Slide[]) => {
+      if(slides.length > 0 && slides[0].viewportRatio) {
+        slidesStore.setViewportRatio(slides[0].viewportRatio)
+      }
       slidesStore.setSlides(slides)
     })
   }
